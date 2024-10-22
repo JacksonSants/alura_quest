c12f066bce3c9c84fc0cff70751db1193a4b1cc7
Jackson Diego <122409790+JacksonSants@users.noreply.github.com>
2024-10-22 11:21:34 -0400

    Update alura_quest.dart
    Instância das classes Wizard e Warrior.

--- bin/alura_quest.dart
+++ bin/alura_quest.dart
@@ -1,43 +1,13 @@
+import '../model/Warrior.dart';
+import '../model/Wizard.dart';
+
 void main() {
-  String name = "Cleitin";
-  String race = "Elf";
-  String classPerson = "Weizard";
+  Wizard megumi = new Wizard(
+      "Megumi", "Humano", "Arquimago", 14, 1.50, true, 500, ["Explosão"], 10000, "cajado");
+  megumi.ShowRecord();
+  megumi.CastSpell();
 
-  int age = 20;
-  double height = 1.80;
-
-  bool indication = true;
-
-  int lifePoint = 100;
-  int mp = 100;
-
-  List<String> skills = ["Fireball", "BlackFire"];
-
-  print("==== Característica do personagem ====\n");
-  print("Nome: $name\n"
-      "Raça: $race\n"
-      "Classe: $classPerson\n"
-      "Idade: $age\n"
-      "Altura: $height\n"
-      "Indicadores: $indication\n"
-      "Pontos de vida: $lifePoint\n"
-      "Pontos de magia: $mp\n"
-      "Habilidades:");
-
-  for (var i = 0; i < skills.length; i++) {
-    print("Habilidade ${i + 1}:  ${skills[i]}");
-  }
-
-  print("\n\nVocê está em uma batalha real!");
-  print("Você tem $mp pontos de magia");
-  print("Você tem as habilidades: ");
-  for (var i = 0; i < skills.length; i++) {
-    print("Habilidade ${i + 1}:  ${skills[i]}");
-  }
-  print("\nBattle Royale");
-  print("Começar a batalha!!\n");
-  while (lifePoint > 0) {
-    print("Pontos de vida:  $lifePoint");
-    lifePoint -= 10;
-  }
+  Warrior bell = new Warrior(
+      "Bell Crunell", "Humano", "Guerreiro", 14, 1.65, true, 1000, ["Fireball", "Liaris Freese", "Ox Slayer", "Argonaut", "Faca de Hestia"], 3000, 500, "Gatuno");
+  bell.ShowRecord();
 }
